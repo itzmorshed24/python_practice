4fdc33b51b492eb8f7347a4cdf6bcea9151821d3
itzmorshed24 <itzmorshed24@gmail.com>
2025-08-15 08:47:34 +0600

    excercise list example

---
+++ 03_list/excercie/excercise-1.py
@@ -0,0 +1,29 @@
+#Number1
+country_name = ['India', 'Pakistan', 'Australia', 'Srilanka', 'Bangladesh']
+print(country_name)
+
+#Number2
+print(country_name[0])
+print(country_name[-1])
+
+#Number3
+country_name.append('England')
+print(country_name)
+
+#number4
+country_name.remove('India')
+print(country_name)
+
+#Number5
+result = [80, 85, 75, 65, 48, 95, 43, 48, 52, 85, 75]
+result.sort()
+print(result)
+
+#Number6
+nested = [[1,2], [3,4], [5,6], [7,8]]
+print(nested[2][1])
+
+#Number7
+fruits = ['apple', 'orange', 'jackfruits', 'apple', 'lemon', 'apple']
+apple_count = fruits.count('apple')
+print(apple_count)
