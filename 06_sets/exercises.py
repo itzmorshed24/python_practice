81df7f0dd593021ad9c73b2cf262974ae45abb1e
itzmorshed24 <itzmorshed24@gmail.com>
2025-08-21 07:43:22 +0600

    excercise set data type

---
+++ 06_sets/exercises.py
@@ -0,0 +1,17 @@
+a = {1, 2, 3, 4, 5}
+print(a)
+
+a.add(6)
+print(a)
+
+a.remove(3)
+print(a)
+
+set_one = { 2, 4, 6, 8, 10}
+set_two = {1, 2, 3, 4, 5, 7, 9, 10}
+
+set_union = set_one.union(set_two)
+print(set_union)
+
+set_intersection = set_one.intersection(set_two)
+print(set_intersection)
